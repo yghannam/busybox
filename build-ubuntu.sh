b9876c11e469a592c8eeb81e80f6ee3027ac986d
Yazen Ghannam <yazen.ghannam@linaro.org>
2015-12-05 12:30:44 -0600

    Don't pull image from Docker Hub. Use local image.

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(readlink -f "$(dirname "$BASH_SOURCE")")/ubuntu"
 
 set -x
-docker build -t busybox:ubuntu-builder --pull - < Dockerfile.builder
+docker build -t busybox:ubuntu-builder - < Dockerfile.builder
 docker run --rm busybox:ubuntu-builder tar c . | xz -z9 > busybox.tar.xz
 docker build -t busybox:ubuntu .
 docker run --rm busybox:ubuntu sh -xec 'true'
